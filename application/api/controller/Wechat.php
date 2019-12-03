d213c5db18d2ca892f6ea6d840410e20c4d9f0aa
王威 <10220739@qq.com>
2019-12-03 13:26:44 +0800

    Signed-off-by: 王威 <10220739@qq.com>

--- application/api/controller/Wechat.php
+++ application/api/controller/Wechat.php
@@ -12,15 +12,16 @@ class Wechat extends Api {
     protected $noNeedRight = ['*'];
 	
 	public $appid = 'wx370d0f595f75aff2';
-	public $redirect_uri = '';
+	public $redirect_uri = 'http://www.culiu.xyz/api/wechat/getToken';
 	
     
 	public function getCode(){
-		$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . $this->appid . "&redirect_uri=". $this->redirect_uri ."&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
+		$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . $this->appid . "&redirect_uri=". $this->redirect_uri ."&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
+		echo $url;
 	}
 	
 	
-	public function requestUser(){
+	public function getToken(){
 		
 	}
 	
